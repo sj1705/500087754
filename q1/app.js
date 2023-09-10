8cfedeb06d47e5e545cf4d34d4b4ad92f5c33252
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-09-10 12:33:32 +0530

    Changes to be committed: 	modified:   q1/app.js

--- q1/app.js
+++ q1/app.js
@@ -4,33 +4,31 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQzMjkyMTQsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDdlNTI3YTktMWIwMS00Mzc0LWJiYmMtMTk3NGUzZTA3ZWZkIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjUwMDA4Nzc1NCJ9.tEeEdiQyB3Z02E4JCtFJuPZynYmjYyJC5rYbk1N_xYY';
+app.use(express.json());
 
 app.get('/train-schedule', async (req, res) => {
     try {
-        const response = await axios.get('http://20.244.56.144/train/trains', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-
-        if (response.status === 200) {
-            const data = response.data;
-
-            const sortedTrains = sortAndFilter(data);
-            res.json(sortedTrains);
-        } else {
-            res.status(500).json({ error: 'Unable to fetch train schedule' });
-        }
+        const trains = await fetchTrainData();
+        const filteredTrains = filterAndSortTrains(trains);
+        res.json(filteredTrains);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred' });
     }
 });
 
-function sortAndFilter(data) {
-// i need to implement
-    return sortedData;
+async function fetchTrainData() {
+    const response = await axios.get('http://20.244.56.144/train/trains', {
+        headers: {
+            Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQzMjkyMTQsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDdlNTI3YTktMWIwMS00Mzc0LWJiYmMtMTk3NGUzZTA3ZWZkIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjUwMDA4Nzc1NCJ9.tEeEdiQyB3Z02E4JCtFJuPZynYmjYyJC5rYbk1N_xYY',
+        },
+    });
+    return response.data;
+}
+
+function filterAndSortTrains(trains) {
+
+    return filteredAndSortedTrains;
 }
 
 app.listen(port, () => {
