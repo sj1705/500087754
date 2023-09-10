a57d5fdd5c41ce6ae341a20a2b654d746b698521
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-09-10 12:59:07 +0530

    Changes to be committed: 	modified:   q1/app.js

--- q1/app.js
+++ q1/app.js
@@ -9,8 +9,9 @@ app.use(express.json());
 app.get('/train-schedule', async (req, res) => {
     try {
         const trains = await fetchTrainData();
-        const filteredTrains = filterAndSortTrains(trains);
-        res.json(filteredTrains);
+        // const filteredTrains = filterAndSortTrains(trains);
+        // res.json(filteredTrains);
+        console.log(trains);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred' });
@@ -27,9 +28,51 @@ async function fetchTrainData() {
 }
 
 function filterAndSortTrains(trains) {
+    const currentTime = new Date();
+    const twelveHoursLater = new Date(currentTime.getTime() + 12 * 60 * 60 * 1000);
 
-    return filteredAndSortedTrains;
+    const filteredTrains = trains.filter((train) => {
+        const departureTime = new Date();
+        departureTime.setHours(train.departureTime.hours);
+        departureTime.setMinutes(train.departureTime.minutes);
+        departureTime.setSeconds(train.departureTime.seconds);
+
+        return departureTime > twelveHoursLater;
+    });
+
+    const sortedTrains = filteredTrains.sort((a, b) => {
+        const priceA = a.price.sleeper + a.price.AC;
+        const priceB = b.price.sleeper + b.price.AC;
+        if (priceA !== priceB) {
+            return priceA - priceB;
+        }
+
+        const ticketsA = a.seatsAvailable.sleeper + a.seatsAvailable.AC;
+        const ticketsB = b.seatsAvailable.sleeper + b.seatsAvailable.AC;
+        if (ticketsA !== ticketsB) {
+            return ticketsB - ticketsA;
+        }
+
+        const departureTimeA = calculateDelayedDepartureTime(a);
+        const departureTimeB = calculateDelayedDepartureTime(b);
+
+        return departureTimeB - departureTimeA;
+    });
+
+    return sortedTrains;
 }
+
+function calculateDelayedDepartureTime(train) {
+    const departureTime = new Date();
+    departureTime.setHours(train.departureTime.hours);
+    departureTime.setMinutes(train.departureTime.minutes);
+    departureTime.setSeconds(train.departureTime.seconds);
+
+    departureTime.setMinutes(departureTime.getMinutes() + train.delayedBy);
+
+    return departureTime;
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
